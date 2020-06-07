7b50a5f599869050e8615bb86fa0d2d4ed05903f
mariuscomas <mariuscr@me.com>
2020-06-07 10:40:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tryit191
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tryit191)
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+
+@Component ({
+
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+
+})
+
+export class ProductDetailsComponent{
+
+
+
+}
